6c2783b5a9ab70beb9950f87c2607fcc65893adc
Naren Vilva <51068682+ShadyCookies@users.noreply.github.com>
2021-03-19 21:44:23 +0530

    Update HLR_BM1.m

--- HLR_BM1.m
+++ HLR_BM1.m
@@ -5,8 +5,8 @@ A = int32(A);
 B = int32(B);
 dm1 = 0;
 
-for index=0:3:6
-    dp2 = bitget(B,bin_index(index+2));
+for index=0:3:6                                 % Using R8ABE1 for the first 3 partial product rows
+    dp2 = bitget(B,bin_index(index+2));         % Getting the 4 bit combination from the multiplier D
     dp1 = bitget(B,bin_index(index+1));
     d = bitget(B,bin_index(index));
     
@@ -33,9 +33,9 @@ for index=0:3:6
     dm1 = dp2;
 end
 
-for index=9:2:15
+for index=9:2:15                                % Using Accurate Radix 4 booth for the rest of the partial product rows
     
-    dp1 = bitget(B,bin_index(index+1));
+    dp1 = bitget(B,bin_index(index+1));         % Getting the 3 bit combination from the multiplier D          
     d = bitget(B,bin_index(index));
     dm1 = bitget(B,bin_index(index-1));
     
